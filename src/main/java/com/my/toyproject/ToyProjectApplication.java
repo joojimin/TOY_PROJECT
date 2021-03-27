365fdcd1b91160b555e9f38103a2bf2437e1534f
jiminJoo <jimin.joo@nhnsoft.com>
2021-03-27 15:19:18 +0900

    [ADD] RequestMapping log

--- src/main/java/com/my/toyproject/ToyProjectApplication.java
+++ src/main/java/com/my/toyproject/ToyProjectApplication.java
@@ -1,13 +1,24 @@
 package com.my.toyproject;
 
+import com.my.toyproject.spring.service.PrintRequestMappingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class ToyProjectApplication {
+
+    private final PrintRequestMappingService printRequestMappingService;
 
     public static void main(String[] args) {
         SpringApplication.run(ToyProjectApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(){
+        return printRequestMappingService.run();
+    }
 }
