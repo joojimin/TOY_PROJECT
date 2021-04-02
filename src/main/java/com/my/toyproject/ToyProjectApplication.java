ef3aae763704cc9ebeaebf641a5b273015b098e8
jimin.joo <wnwlals22@naver.com>
2021-04-03 00:58:45 +0900

    [UPDATE] PrintRequestMapping service 적용 위치 변경

--- src/main/java/com/my/toyproject/ToyProjectApplication.java
+++ src/main/java/com/my/toyproject/ToyProjectApplication.java
@@ -1,30 +1,16 @@
 package com.my.toyproject;
 
-import com.my.toyproject.printrequest.service.PrintRequestMappingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 
 @RequiredArgsConstructor
 @SpringBootApplication
 @ServletComponentScan(basePackages = "com.my.toyproject.spring")
 public class ToyProjectApplication {
 
-    private final PrintRequestMappingService printRequestMappingService;
-
     public static void main(String[] args) {
         SpringApplication.run(ToyProjectApplication.class, args);
-    }
-
-    /**
-     * Print request mapping method
-     * @return
-     */
-    @Bean
-    public CommandLineRunner commandLineRunner(){
-        return printRequestMappingService.run();
     }
 }
