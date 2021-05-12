6885bd18c8df364c359160b9bb6740209dea9519
jimin.joo <wnwlals22@naver.com>
2021-05-12 19:29:08 +0900

    feat: default mybatis typehandler 확인

--- src/main/java/com/my/toyproject/shop/dto/MemberDto.java
+++ src/main/java/com/my/toyproject/shop/dto/MemberDto.java
@@ -1,15 +1,20 @@
 package com.my.toyproject.shop.dto;
 
+import com.my.toyproject.shop.type.MemberBelongs;
 import lombok.*;
 
+import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Builder
-@ToString(of = {"memberId", "memberName", "memberAddress"})
+@ToString(of = {"memberId", "memberName", "memberAddress", "memberBelongs"})
 public class MemberDto {
     private int memberId;
     private String memberName;
+    private MemberBelongs memberBelongs;
     private String memberAddress;
+    private LocalDateTime registerTime;
+    private LocalDateTime updateTime;
 }
